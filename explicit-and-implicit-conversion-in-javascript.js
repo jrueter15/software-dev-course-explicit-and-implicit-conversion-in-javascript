9c7a14e7d74b6ba45461bdf10dde2bd5bc00c923
jrueter15 <jrueter15@gmail.com>
2025-03-19 20:06:39 -0500

    Corrected Bugs and Added New Conversions

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -19,14 +19,25 @@ Use console.log() to clearly show the before-and-after type conversions.
 */
 
 
-let result = "5" - 2;
+let result = Number("5") - 2; // 5 was a string, so we converted it to a number
 console.log("The result is: " + result);
 
-let isValid = Boolean("false");
+let isValid = true; // changing the string "false" to a boolean value yielded true 
 if (isValid) {
     console.log("This is valid!");
 }
 
-let age = "25";
+let age = Number("25"); // 25 was a string, so we converted it to a number
 let totalAge = age + 5;
 console.log("Total Age: " + totalAge);
+
+console.log(10 + true); // true is converted to 1 then added to 10
+
+let teamNum = "68"
+let convertedNum = Number(teamNum);
+let winner = convertedNum - 67;
+console.log("There can only be: " + winner + " winner!");
+
+console.log(null > 0);  // null is converted to 0
+console.log(null == 0); // false, null is not equal to 0
+console.log(null >= 0); // true, null is converted to 0
